35049da07192a8abe6c20ca4610e178c2e2f1c70
shane-decatur <sdecatur20@stuy.edu>
2021-10-18 23:31:04 -0400

    added slightly more testing

--- main.c
+++ main.c
@@ -9,14 +9,31 @@ int main(){
   struct con *s = make(100,"wizard");
   s = insert_front(s,12,"knight");
   s = insert_front(s,0,"court jester");
+  s = insert_front(s,80,"necromancer");
+  s = insert_front(s,1000,"huge nerd");
   print_list(s);
 
-  s = remove_node(s, 0);
-  print_list(s);
-  // printf("%p\n",s);
-  // printf("%p\n",free_list(s));
+  printf("\n");
 
-  // struct con *x = make(12,"knight");
-  // print_list(x);
-  // free(x);
+  //removes in middle of list
+  s = remove_node(s, 12);
+  print_list(s);
+
+  printf("\n");
+
+  //removes beginning of list
+  s = remove_node(s,1000);
+  print_list(s);
+
+  printf("\n");
+
+  //removes end of list
+  s = remove_node(s,100);
+  print_list(s);
+
+  printf("\n");
+
+  //removes nonexistent value
+  s = remove_node(s,1209);
+  print_list(s);
 }
